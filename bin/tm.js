55d2bd78b98557dc377deb36a0361520b3e36cdb
Tao Cheng <taocheng@qnap.com>
2018-02-02 18:42:02 +0800

    WIP: tm.js

--- bin/tm.js
+++ bin/tm.js
@@ -2,30 +2,20 @@
 
 /************** Constant strings **************/
 
-const MISSING_DEP = `A missing dependency, '%s', is detected. Do you want to enable auto-installation?`
+const MISSING_DEP = `Missing dependenc%s detected. Do you want to enable auto-installation?`
 
 /**********************************************/
 
-const util = require('util')
-const require = wrapRequire()
-
-require('validate-npm-package-name')
+// const {} = getDependencies()
 
 /******* Function stack starts from here ******/
 
-function wrapRequire () {
-  let wrapper = function (pkg) {
-    try {
-      return wrapper.require(pkg)
-    } catch (err) {
-      prompt(util.format(MISSING_DEP, pkg), 'yes', function (ans) {
-      })
-    }
-  }
-  wrapper.require = require
+// /**
+//  * @return {object} all dependencies
+//  */
+// function getDependencies () {
 
-  return wrapper
-}
+// }
 
 function prompt (msg, defaultValue, validator = function () {}) {
   let displayedDefault
