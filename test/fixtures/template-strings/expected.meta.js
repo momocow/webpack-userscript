56498a5e4985a141348126aec9db55f43bf129d0
MomoCow <taocheng@qnap.com>
2019-04-01 10:55:28 +0100

    :white_check_mark: Add a test for build number.

--- test/fixtures/template-strings/expected.meta.js
+++ test/fixtures/template-strings/expected.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        WPUS: @webpack-tampermonkey/test-template-strings
-// @version     1.0.0-build.d4a1f0a87ad337e02d07
+// @version     1.0.0-build.1.d4a1f0a87ad337e02d07
 // @author      MomoCow
 // @description Template strings test for webpack-userscript.
 // @homepage    https://github.com/momocow/webpack-tampermonkey#readme
