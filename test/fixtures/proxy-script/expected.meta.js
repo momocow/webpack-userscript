757dbf5888b0d3f90389fd55d905548bb9901a58
MomoCow <momocow.me@gmail.com>
2019-11-26 22:17:37 +0800

    :white_check_mark: Add a test for proxy script

---
+++ test/fixtures/proxy-script/expected.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Proxy Script Test
+// @version 2.0.0
+// @match https://google.com/*
+// @grant GM_setValue
+// @grant GM_getValue
+// @require http://127.0.0.1:12345/index.user.js
+// ==/UserScript==
---
+++ test/fixtures/proxy-script/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const WebpackUserscript = require('../../..')
+
+module.exports = {
+  mode: 'production',
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'output'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new WebpackUserscript({
+      headers: {
+        name: 'Proxy Script Test',
+        version: '2.0.0',
+        match: 'https://google.com/*',
+        grant: [
+          'GM_setValue',
+          'GM_getValue'
+        ]
+      },
+      pretty: false,
+      proxyScript: {
+        baseUrl: 'http://127.0.0.1:12345',
+        filename: 'tm-no-cache.proxy.user.js',
+        enable: true
+      }
+    })
+  ]
+}
