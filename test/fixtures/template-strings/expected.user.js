d1224a0519b014c196d31b2dfa0ab260e22c701f
MomoCow <momocow.me@gmail.com>
2020-01-18 00:09:42 +0800

    :bug: Fix no updateURL produced if option metajs is false.
    updateURL should be the same as downloadURL in this case.

--- test/fixtures/template-strings/expected.user.js
+++ test/fixtures/template-strings/expected.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        WPUS: @webpack-tampermonkey/test-template-strings
-// @version     1.0.0-build.1.d4a1f0a87ad337e02d07
+// @version     1.0.0-build.1.a35aa354121e7a86358f
 // @author      MomoCow
 // @description Template strings test for webpack-userscript.
 // @homepage    https://github.com/momocow/webpack-tampermonkey#readme
