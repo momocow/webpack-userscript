0c776bdd086ecf966b3b0951e68777b9ec85c9f1
MomoCow <momocow.me@gmail.com>
2023-01-31 02:48:03 +0800

    :wrench: update project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './eslint.tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
