c8d561d02b2db71cfd0fa1328f7bd690b1f2ee45
MomoCow <momocow.me@gmail.com>
2023-01-30 22:01:29 +0800

    :wrench: add dev-specific tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './eslint.tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
