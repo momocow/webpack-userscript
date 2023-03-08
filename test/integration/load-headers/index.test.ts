e351229d9d7fca506958224440fb92c03a8aad0e
MomoCow <momocow.me@gmail.com>
2023-03-08 17:14:35 +0800

    :white_check_mark: add a test for reusing headers file

--- test/integration/load-headers/index.test.ts
+++ test/integration/load-headers/index.test.ts
@@ -1,6 +1,6 @@
 import { HeadersProps, UserscriptPlugin } from 'webpack-userscript';
 
-import { compile } from '../util';
+import { compile, watchCompile } from '../util';
 import { Volume } from '../volume';
 import { Fixtures } from './fixtures';
 
@@ -32,37 +32,96 @@ describe('load-headers', () => {
     });
   });
 
-  it('can be loaded from headers file', async () => {
-    input.writeFileSync('/headers.json', Fixtures.headersJson);
+  describe('headers file', () => {
+    it('can be loaded from headers file', async () => {
+      input.writeFileSync('/headers.json', Fixtures.headersJson);
 
-    const output = await compile(input, {
-      ...Fixtures.webpackConfig,
-      plugins: [
-        new UserscriptPlugin({
-          headers: '/headers.json',
-        }),
-      ],
+      const output = await compile(input, {
+        ...Fixtures.webpackConfig,
+        plugins: [
+          new UserscriptPlugin({
+            headers: '/headers.json',
+          }),
+        ],
+      });
+
+      expect(output.toJSON()).toEqual({
+        '/dist/output.user.js': Fixtures.entryUserJs(
+          Fixtures.loadHeadersHeaders,
+        ),
+        '/dist/output.meta.js': Fixtures.loadHeadersHeaders,
+      });
     });
 
-    expect(output.toJSON()).toEqual({
-      '/dist/output.user.js': Fixtures.entryUserJs(Fixtures.loadHeadersHeaders),
-      '/dist/output.meta.js': Fixtures.loadHeadersHeaders,
-    });
-  });
+    // eslint-disable-next-line max-len
+    it('should throw error if headers file is not in .json format', async () => {
+      input.writeFileSync('/headers.json', '{"name": "invalid-json",');
 
-  it('should throw error if headers file is not in .json format', async () => {
-    input.writeFileSync('/headers.json', '{"name": "invalid-json",');
+      const promise = compile(input, {
+        ...Fixtures.webpackConfig,
+        plugins: [
+          new UserscriptPlugin({
+            headers: '/headers.json',
+          }),
+        ],
+      });
 
-    const promise = compile(input, {
-      ...Fixtures.webpackConfig,
-      plugins: [
-        new UserscriptPlugin({
-          headers: '/headers.json',
-        }),
-      ],
+      await expect(promise).toReject();
     });
 
-    await expect(promise).toReject();
+    // eslint-disable-next-line max-len
+    it('should reuse from headers file if the file is not changed', async () => {
+      input.writeFileSync('/headers.json', Fixtures.headersJson);
+
+      const plugin = new UserscriptPlugin({
+        headers: './headers.json',
+      });
+
+      const loadFromHeadersFile = jest.spyOn(
+        plugin.features[0],
+        'loadFromHeadersFile' as any,
+      );
+
+      const entry = './entry.js';
+      let step = 0;
+
+      await watchCompile(
+        input,
+        {
+          ...Fixtures.webpackConfig,
+          context: '/',
+          entry,
+          plugins: [plugin],
+        },
+        async ({ output, writeFile }) => {
+          switch (++step) {
+            case 1:
+              await writeFile(entry, Fixtures.entryJs);
+              expect(output.toJSON()).toEqual({
+                '/dist/output.user.js': Fixtures.entryUserJs(
+                  Fixtures.loadHeadersHeaders,
+                ),
+                '/dist/output.meta.js': Fixtures.loadHeadersHeaders,
+              });
+              break;
+
+            case 2:
+              break;
+
+            default:
+              fail('invalid steps');
+          }
+
+          return step < 2;
+        },
+      );
+
+      if (step !== 2) {
+        fail('invalid steps');
+      }
+
+      expect(loadFromHeadersFile).toHaveBeenCalledOnce();
+    });
   });
 
   it('should compile if package.json does not exist', async () => {
--- test/integration/util.ts
+++ test/integration/util.ts
@@ -1,4 +1,6 @@
+import { mkdtemp, rm, writeFile } from 'node:fs/promises';
 import { AddressInfo } from 'node:net';
+import { tmpdir } from 'node:os';
 import path from 'node:path';
 import { promisify } from 'node:util';
 
@@ -9,24 +11,20 @@ import { createFsFromVolume, Volume } from './volume';
 
 export const GLOBAL_FIXTURES_DIR = path.join(__dirname, 'fixtures');
 
+export interface CompilerFileSystems {
+  intermediateFileSystem?: Volume;
+}
+
 export async function compile(
   input: Volume,
-  webpackConfig: Configuration,
-  {
-    inputFileSystem,
-    intermediateFileSystem,
-    outputFileSystem,
-  }: {
-    inputFileSystem?: Volume;
-    intermediateFileSystem?: Volume;
-    outputFileSystem?: Volume;
-  } = {},
+  config: Configuration,
+  { intermediateFileSystem }: CompilerFileSystems = {},
 ): Promise<Volume> {
-  const compiler = webpack(webpackConfig);
-
   const output = new Volume();
-  compiler.inputFileSystem = inputFileSystem ?? createFsFromVolume(input);
-  compiler.outputFileSystem = outputFileSystem ?? createFsFromVolume(output);
+  const compiler = webpack(config);
+
+  compiler.inputFileSystem = createFsFromVolume(input);
+  compiler.outputFileSystem = createFsFromVolume(output);
   compiler.intermediateFileSystem =
     intermediateFileSystem ?? (compiler.outputFileSystem as Volume);
 
@@ -35,6 +33,7 @@ export async function compile(
 
   if (stats?.hasErrors() || stats?.hasWarnings()) {
     const details = stats.toJson();
+
     if (details.errorsCount) {
       console.error(details.errors);
     }
@@ -46,6 +45,101 @@ export async function compile(
   }
 
   return output;
+}
+
+export interface WatchStep {
+  output: Volume;
+  writeFile: (
+    file: string,
+    body: string,
+    encode?: BufferEncoding,
+  ) => Promise<void>;
+}
+
+export async function watchCompile(
+  input: Volume,
+  config: Configuration,
+  handle: (ctx: WatchStep) => Promise<boolean>,
+): Promise<void> {
+  return new Promise<void>(async (resolve, reject) => {
+    const watchDir = await mkdtemp(
+      path.join(tmpdir(), 'webpack-userscript_test_'),
+    );
+
+    for (const [file, body] of Object.entries(input.toJSON())) {
+      if (body === null) {
+        continue;
+      }
+
+      await writeFile(path.join(watchDir, file), body, 'utf-8');
+    }
+
+    const output = new Volume();
+    const compiler = webpack({
+      ...config,
+      context:
+        typeof config.context === 'string'
+          ? path.join(watchDir, config.context)
+          : undefined,
+    });
+
+    compiler.outputFileSystem = createFsFromVolume(output);
+
+    const watching = compiler.watch({}, async (err, stats) => {
+      if (err) {
+        await close();
+        reject(err);
+
+        return;
+      }
+
+      if (stats?.hasErrors() || stats?.hasWarnings()) {
+        const details = stats.toJson();
+
+        if (details.errorsCount) {
+          console.error(details.errors);
+        }
+        if (details.warningsCount) {
+          console.error(details.warnings);
+        }
+
+        await close();
+        reject(new Error('invalid fixtures'));
+
+        return;
+      }
+
+      try {
+        const conti = await handle({
+          output,
+          writeFile: writeFileInWatchDir,
+        });
+
+        if (!conti) {
+          await close();
+          resolve();
+        }
+      } catch (err) {
+        await close();
+        reject(err);
+      }
+    });
+
+    const closeWatching = promisify(watching.close.bind(watching));
+    const closeCompiler = promisify(compiler.close.bind(compiler));
+
+    const close = async (): Promise<void> => {
+      await closeWatching();
+      await closeCompiler();
+      await rm(watchDir, { recursive: true, force: true });
+    };
+
+    const writeFileInWatchDir = (
+      file: string,
+      body: string,
+      encode?: BufferEncoding,
+    ): Promise<void> => writeFile(path.join(watchDir, file), body, encode);
+  });
 }
 
 export interface ServeStatic {
