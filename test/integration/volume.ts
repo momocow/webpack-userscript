10759b152ce2e012a5a15613d6596ffc2763ad14
MomoCow <momocow.me@gmail.com>
2024-12-23 14:16:13 +0800

    :white_check_mark: Fix typings in tests

--- test/integration/volume.ts
+++ test/integration/volume.ts
@@ -1,2 +1,9 @@
-export { createFsFromVolume } from 'memfs';
-export { Volume } from 'memfs/lib/volume';
+import { createFsFromVolume as memfsCreateFsFromVolume } from 'memfs';
+import { Volume } from 'memfs/lib/volume';
+import { InputFileSystem, OutputFileSystem } from 'webpack';
+
+export const createFsFromVolume = memfsCreateFsFromVolume as unknown as (
+  ...args: Parameters<typeof memfsCreateFsFromVolume>
+) => Volume & InputFileSystem & OutputFileSystem;
+
+export { Volume };
